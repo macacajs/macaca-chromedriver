2ac3db0262ec63b9a8266edd268a09d107453a1f
xudafeng <xudafeng@126.com>
2016-05-04 10:47:17 +0800

    tweak tc

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ test: install
 	@node --harmony \
 		${npm_bin}/istanbul cover ${npm_bin}/_mocha \
 		-- \
-		--timeout 10000 \
+		--timeout 20000 \
 		--require co-mocha
 travis: install
 	@NODE_ENV=test $(BIN) $(FLAGS) \
--- test/macaca-chromedriver.test.js
+++ test/macaca-chromedriver.test.js
@@ -13,10 +13,29 @@
 
 'use strict';
 
-var Chromedriver = require('..');
+var ChromeDriver = require('..');
 
 describe('test', function() {
   it('should be ok', function() {
-    Chromedriver.should.be.ok;
+    ChromeDriver.should.be.ok;
+  });
+
+  it('should start success', function *(done) {
+    var chromedriver = new ChromeDriver();
+
+    chromedriver.should.be.ok;
+
+    try {
+      yield chromedriver.killAll();
+    } catch (e) {
+      console.log(e);
+    }
+
+    chromedriver.on(ChromeDriver.EVENT_READY, data => {
+      console.log(`chromedriver ready with: ${JSON.stringify(data)}`);
+      done();
+    });
+
+    chromedriver.start({});
   });
 });
